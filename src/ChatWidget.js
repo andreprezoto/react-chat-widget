c629b444ba414e53adb7bcb980c89e54b3ac3979
andreprezo <andre.prezoto@gmail.com>
2024-09-30 09:19:56 -0300

    Change button color and add loading spinner

--- src/ChatWidget.js
+++ src/ChatWidget.js
@@ -5,6 +5,7 @@ const ChatWidget = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [showBalloon, setShowBalloon] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!isOpen) {
@@ -33,6 +34,31 @@ const ChatWidget = ({
     </svg>
   );
 
+  const Spinner = () => (
+    <div className="flex justify-center items-center h-full">
+      <svg
+        className="animate-spin h-10 w-10 text-[#FF7000]"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+  );
+
   return (
     <div className="fixed bottom-4 right-4 flex items-end">
       {!isOpen && (
@@ -47,7 +73,7 @@ const ChatWidget = ({
           )}
           <button
             onClick={() => setIsOpen(true)}
-            className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-3 shadow-lg"
+            className="bg-[#FF7000] hover:bg-[#FF7000] text-white rounded-full p-3 shadow-lg"
           >
             <MessageIcon />
           </button>
@@ -58,11 +84,14 @@ const ChatWidget = ({
           style={{ width: "500px", height: "600px" }}
           className="bg-white rounded-lg shadow-xl flex flex-col"
         >
+          {isLoading && <Spinner />}
           <iframe
             src="https://app.3mindtecnologia.com.br/version-01bgy/widget_chat"
             title="Chat"
             className="w-full h-full rounded-lg"
             frameBorder="0"
+            onLoad={() => setIsLoading(false)}
+            style={{ display: isLoading ? "none" : "block" }}
           ></iframe>
           <button
             onClick={() => setIsOpen(false)}
