e4ce50c3fe537748beebc888d6efe7bd9f12f309
andreprezo <andre.prezoto@gmail.com>
2024-09-30 09:34:08 -0300

    Fix custom color implementation for button and spinner-2

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,18 @@
-odule.exports = {
+const path = require("path");
+
+module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        "@": path.resolve(__dirname, "src"),
+      };
+      return webpackConfig;
     },
   },
 };
